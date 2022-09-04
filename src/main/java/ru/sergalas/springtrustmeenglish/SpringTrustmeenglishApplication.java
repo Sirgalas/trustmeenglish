487dbac526df26ea5c327db4e9fd102ff879a9a9
sergalas <hudos@rambler.ru>
2022-09-04 22:55:04 +0300

    init commit

---
+++ src/main/java/ru/sergalas/springtrustmeenglish/SpringTrustmeenglishApplication.java
@@ -0,0 +1,13 @@
+package ru.sergalas.springtrustmeenglish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTrustmeenglishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTrustmeenglishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sergalas/springtrustmeenglish/SpringTrustmeenglishApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sergalas.springtrustmeenglish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTrustmeenglishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
